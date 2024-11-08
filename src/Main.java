48fa3ed8cc84a34788b9208f0bc8d967438ec24b
1 <vasilijnenastev@gmail.com>
2024-11-08 18:45:22 +0400

    HW10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Строки задача №1");
+        System.out.println();
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName+" " + firstName+" " + middleName;
+        System.out.print("Ф.И.О. сотрудника " + fullName);
+        System.out.println();
+
+        System.out.print("Строки задача №2");
+        System.out.println();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - "+fullName.toUpperCase());
+
+        System.out.print("Строки задача №3");
+        System.out.println();
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replace("ё", "е");
+        System.out.print("Данные Ф.И.О. сотрудника - "+fullName1);
+
+
+
+
 
 
 
